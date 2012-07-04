d4cd6222a4adf4ae6be316baac75ac84817f4666
df9ry <Tania.Knoebl@Discovery-Design.de>
2012-07-04 19:07:59 +0200

    Initial checkin, it is only a window

---
+++ OpenAX25-GUI/Main.cs
@@ -0,0 +1,16 @@
+using System;
+using Gtk;
+
+namespace OpenAX25GUI
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.Show ();
+			Application.Run ();
+		}
+	}
+}
